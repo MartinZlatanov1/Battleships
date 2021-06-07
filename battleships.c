c26e4889df630599e46fcd1d95675e62300ce2d4
allks12 <66909315+allks12@users.noreply.github.com>
2021-06-07 15:55:21 +0300

    Add files via upload

--- battleships.c
+++ battleships.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
-{
+struct node_t{
+	struct node_t* left;
+	struct node_t* right;
+	struct node_t* up;
+	struct node_t* down;
+	
+	int valid;
+	char ship;
+};
 
-    return 0;
+struct node_t* create_board(){
+	
+}
+
+void search_find(struct node_t* head, int x, int y){
+	for(int i = 1; i < x; i++){
+		head = head->right;	
+	}
+	
+	for(int i = 1; i < y; i++){
+			head = head->down;
+	}
+}
+
+int main() {
+	
+	
+	
+	return 0;
 }
